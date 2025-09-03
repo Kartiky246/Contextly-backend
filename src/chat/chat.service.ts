9f778dd3ac2eeb36c00796f46359f37445730dad
kartiky246 <kartiky246@gmail.com>
2025-09-03 22:01:36 +0530

    fix(): send createdAt timestamp for messages

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -15,7 +15,7 @@ export class ChatService {
 
     async getChatHistory(sessionId: string, userId: string) {
         return this.chatModel
-            .find({ sessionId, userId }, { content: 1, role: 1, _id: 0 })
+            .find({ sessionId, userId }, { content: 1, createdAt: 1, role: 1, _id: 0 })
             .sort({ timeStamp: 1 })
             .lean()
             .exec()
